f51d75a1e88cb6d6e9820785b503860c5c35709e
Derek Brooks <derek@broox.com>
2020-05-09 10:34:23 -0500

    cleanup unused router code

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,9 +6,6 @@ import Home from "../views/Home.vue";
 import NotFound from "../views/NotFound.vue";
 import Search from "../views/Search.vue";
 import Tag from "../views/Tag.vue";
-// import Gallery from "../components/Gallery.vue";
-// import HeaderSearch from "../components/HeaderSearch.vue";
-// import HeaderTitle from "../components/HeaderTitle.vue";
 
 Vue.use(VueRouter);
 
@@ -17,25 +14,21 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
-    //   components: { header: HeaderSearch, content: Gallery }
   },
   {
     path: "/search/:query",
     name: "Search",
     component: Search
-    // components: { header: HeaderSearch, content: Gallery }
   },
   {
     path: "/tagged/:tag",
     name: "Tag",
     component: Tag
-    // components: { header: HeaderTitle, content: Gallery }
   },
   {
     path: "/:album",
     name: "Album",
     component: Album
-    // components: { header: HeaderTitle, content: Gallery }
   },
   { path: "*", name: "404", component: NotFound }
 ];
