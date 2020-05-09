5fdf457bb209bafdc6d64ce827881d8f6cd656b7
Derek Brooks <derek@broox.com>
2020-05-09 10:34:45 -0500

    only load 1 page at a time

--- client/src/models/photo.js
+++ client/src/models/photo.js
@@ -1,12 +1,17 @@
 import { buildQueryString } from "../utils.js";
 
 export default {
+  // async fetchIndex(options) {
   fetchIndex(options) {
     const defaults = {
       limit: 40,
       offset: 0
     };
     const params = Object.assign(defaults, options);
+
+    // const response = await fetch('/api/v1/photos?' + buildQueryString(params));
+    // return response.json();
+
     return fetch('/api/v1/photos?' + buildQueryString(params))
       .then(response => {
         return response.json();
